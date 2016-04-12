ea35136718383301840d744c52eff2a0e7190d8e
Dmitry Fomin <fomin.dm@gmail.com>
2016-04-12 11:07:37 -0400

    Sending and receiving messages.

--- server.go
+++ server.go
@@ -3,17 +3,41 @@ package main
 import (
     "fmt"
     "net/http"
+    "net/http/httputil"
+    "io/ioutil"
+    "net/url"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "qwe")
-    fmt.Println(r)
+    data, _ := httputil.DumpRequest(r, true)
+    fmt.Printf("%s\n\n", data)
+
+    sendMessage()
+}
+
+func sendMessage() {
+    apiUrl := `https://api.telegram.org/bot120816766:AAHuy66RPZLVt3JwBWPwGh2Ndxt_KwAXYlE/sendMessage`
+    data := url.Values{}
+    data.Add("chat_id", "45227519")
+    data.Add("text", "что в итоге?")
+
+    resp, err := http.Get(apiUrl + "?" + data.Encode())
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
 }
 
 func main() {
     http.HandleFunc("/tstkbot", handler)
-    err := http.ListenAndServeTLS(":8443", "/etc/letsencrypt/live/pigowl.com/fullchain.pem", "/etc/letsencrypt/live/pigowl.com/privkey.pem", nil)
+    err := http.ListenAndServeTLS(":8443", "fullchain.pem", "privkey.pem", nil)
     if err != nil {
         fmt.Println("ListenAndServe: ", err)
     }
 }
+
