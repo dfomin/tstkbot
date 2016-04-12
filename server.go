fc64e01fa95056a1199784ad6d3db22c1271e0e1
Dmitry Fomin <fomin.dm@gmail.com>
2016-04-12 18:38:21 -0400

    Echo answering. Send sticker for /punto

--- server.go
+++ server.go
@@ -3,34 +3,72 @@ package main
 import (
     "fmt"
     "net/http"
-    "net/http/httputil"
-    "io/ioutil"
+    //"net/http/httputil"
     "net/url"
+    "encoding/json"
+    "strconv"
+    "strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    data, _ := httputil.DumpRequest(r, true)
-    fmt.Printf("%s\n\n", data)
+const (
+    pigFileId = "BQADAgAD6AAD9HsZAAF6rDKYKVsEPwI"
+)
 
-    sendMessage()
+type Chat struct {
+    Id int `json:"id"`
 }
 
-func sendMessage() {
+type Message struct {
+    Chat Chat `json:"chat"`
+    Text string `json:"text"`
+}
+
+type Object struct {
+    Message Message `json:"message"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    //data, _ := httputil.DumpRequest(r, true)
+    //fmt.Printf("%s\n\n", data)
+    var object Object
+    err := json.NewDecoder(r.Body).Decode(&object)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    index := strings.Index(object.Message.Text, "/punto")
+    if index != -1 {
+        sendSticker(object.Message.Chat.Id, pigFileId)
+    } else {
+        sendMessage(object.Message.Chat.Id, object.Message.Text)
+    }
+}
+
+func sendMessage(id int, text string) {
     apiUrl := `https://api.telegram.org/bot120816766:AAHuy66RPZLVt3JwBWPwGh2Ndxt_KwAXYlE/sendMessage`
     data := url.Values{}
-    data.Add("chat_id", "45227519")
-    data.Add("text", "что в итоге?")
+    data.Add("chat_id", strconv.Itoa(id))
+    data.Add("text", text)
 
     resp, err := http.Get(apiUrl + "?" + data.Encode())
     if err != nil {
-        panic(err)
+        fmt.Println(err)
     }
     defer resp.Body.Close()
+}
 
-    fmt.Println("response Status:", resp.Status)
-    fmt.Println("response Headers:", resp.Header)
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body:", string(body))
+func sendSticker(id int, fileId string) {
+    apiUrl := `https://api.telegram.org/bot120816766:AAHuy66RPZLVt3JwBWPwGh2Ndxt_KwAXYlE/sendSticker`
+    data := url.Values{}
+    data.Add("chat_id", strconv.Itoa(id))
+    data.Add("sticker", fileId)
+
+    //fmt.Println(data.Encode())
+    resp, err := http.Get(apiUrl + "?" + data.Encode())
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer resp.Body.Close()
 }
 
 func main() {
